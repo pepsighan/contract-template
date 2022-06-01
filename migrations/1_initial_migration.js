c4ff3451bb4bb56a973c8003202b4ea2c82926fd
Sharad Chand <pepsighan@sharadchand.com>
2022-06-01 09:04:58 +0530

    Uncontextual comment

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,6 +1,5 @@
 const Contract = artifacts.require('./Contract');
 
-/// Deploy the portals to the respective networks.
 module.exports = async function (deployer) {
   deployer.deploy(Contract);
 };
